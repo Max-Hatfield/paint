d7f79bc9e6d9148d6f5145a3ddb4505f6681f970
Max Hatfield <Max Hatfield@192.168.86.248>
2022-01-31 19:09:24 -0700

    paint

---
+++ project/src/project/work.java
@@ -0,0 +1,14 @@
+package project;
+
+public class work {
+
+	public static void main(String []args)
+	{
+		greetUser("fa");
+	}
+	public static String greetUser(String x)
+	{
+return x;
+		//System.out.println("This quiz is worth " + x + " points.");
+	   }
+} 
